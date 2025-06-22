d28358cf6bac887bb1ab783ab0e3921a8bf6c102
Simant-Asawale-Coding <simant.asawale@aress.com>
2025-06-22 16:21:12 +0530

    Update week-4.py

--- week-4.py
+++ week-4.py
@@ -1,13 +1,4 @@
-"""
-Project for Week 4 of "Python Data Analysis".
-Processing CSV files with baseball stastics.
-
-Be sure to read the project description page for further information
-about the expected behavior of the program.
-"""
-
 import csv
-
 
 def read_csv_fieldnames(filename, separator, quote):
     """
@@ -16,18 +7,13 @@ def read_csv_fieldnames(filename, separator, quote):
       separator - character that separates fields
       quote     - character used to optionally quote fields
     Output:
-      Returns a list of strings corresponding to the field names in
-      the given CSV file.
+      A list of strings corresponding to the field names in the given CSV file.
     """
     with open(filename, newline='', encoding='utf-8') as csvfile:
-        csvreader = csv.DictReader(csvfile,
-                                   delimiter=separator,
-                                   quotechar=quote)
-        return csvreader.fieldnames
-
-##
-## Provided code from Week 3 Project
-##
+        csvreader = csv.reader(csvfile, delimiter=separator, quotechar=quote)
+        for row in csvreader:
+            return row
+    return []
 
 def read_csv_as_list_dict(filename, separator, quote):
     """
@@ -37,7 +23,7 @@ def read_csv_as_list_dict(filename, separator, quote):
       quote     - character used to optionally quote fields
     Output:
       Returns a list of dictionaries where each item in the list
-      corresponds to a row in the CSV file.  The dictionaries in the
+      corresponds to a row in the CSV file. The dictionaries in the
       list map the field names to the field values for that row.
     """
     table = []
@@ -46,8 +32,6 @@ def read_csv_as_list_dict(filename, separator, quote):
         for row in csvreader:
             table.append(row)
     return table
-#stats_list = read_csv_as_list_dict('batting1.csv', ",", '"')
-#print(stats_list)
 
 def read_csv_as_nested_dict(filename, keyfield, separator, quote):
     """
@@ -59,8 +43,7 @@ def read_csv_as_nested_dict(filename, keyfield, separator, quote):
     Output:
       Returns a dictionary of dictionaries where the outer dictionary
       maps the value in the key_field to the corresponding row in the
-      CSV file.  The inner dictionaries map the field names to the
-      field values for that row.
+      CSV file. The inner dictionaries map the field names to the field values for that row.
     """
     table = {}
     with open(filename, newline='', encoding='utf-8') as csvfile:
@@ -69,9 +52,6 @@ def read_csv_as_nested_dict(filename, keyfield, separator, quote):
             rowid = row[keyfield]
             table[rowid] = row
     return table
-#stats_dict = read_csv_as_nested_dict('Batting_2016.csv',  ",", '"')
-#print(stats_dict)
-
 
 def write_csv_from_list_dict(filename, table, fieldnames, separator, quote):
     """
@@ -83,7 +63,7 @@ def write_csv_from_list_dict(filename, table, fieldnames, separator, quote):
       quote      - character used to optionally quote fields
     Output:
       Writes the table to a CSV file with the specified name, using the specified
-      fieldnames, separator, and quote character.
+      fieldnames, separator, and quote character. All non-numeric fields will be quoted.
     """
     with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
         csvwriter = csv.DictWriter(csvfile,
@@ -94,58 +74,3 @@ def write_csv_from_list_dict(filename, table, fieldnames, separator, quote):
         csvwriter.writeheader()
         for row in table:
             csvwriter.writerow(row)
-
-
-##
-## Provided formulas for common batting statistics
-##
-
-# Typical cutoff used for official statistics
-MINIMUM_AB = 500
-
-def batting_average(info, batting_stats):
-    """
-    Inputs:
-      batting_stats - dictionary of batting statistics (values are strings)
-    Output:
-      Returns the batting average as a float
-    """
-    hits = float(batting_stats[info["hits"]])
-    at_bats = float(batting_stats[info["atbats"]])
-    if at_bats >= MINIMUM_AB:
-        return hits / at_bats
-    else:
-        return 0
-
-def onbase_percentage(info, batting_stats):
-    """
-    Inputs:
-      batting_stats - dictionary of batting statistics (values are strings)
-    Output:
-      Returns the on-base percentage as a float
-    """
-    hits = float(batting_stats[info["hits"]])
-    at_bats = float(batting_stats[info["atbats"]])
-    walks = float(batting_stats[info["walks"]])
-    if at_bats >= MINIMUM_AB:
-        return (hits + walks) / (at_bats + walks)
-    else:
-        return 0
-
-def slugging_percentage(info, batting_stats):
-    """
-    Inputs:
-      batting_stats - dictionary of batting statistics (values are strings)
-    Output:
-      Returns the slugging percentage as a float
-    """
-    hits = float(batting_stats[info["hits"]])
-    doubles = float(batting_stats[info["doubles"]])
-    triples = float(batting_stats[info["triples"]])
-    home_runs = float(batting_stats[info["homeruns"]])
-    singles = hits - doubles - triples - home_runs
-    at_bats = float(batting_stats[info["atbats"]])
-    if at_bats >= MINIMUM_AB:
-        return (singles + 2 * doubles + 3 * triples + 4 * home_runs) / at_bats
-    else:
-        return 0

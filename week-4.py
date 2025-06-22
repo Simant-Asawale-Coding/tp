caec128ebf2493234818712b0f456ae543ec6105
Simant-Asawale-Coding <simant.asawale@aress.com>
2025-06-22 16:22:10 +0530

    Update week-4.py

--- week-4.py
+++ week-4.py
@@ -1,19 +1,16 @@
+"""
+Project for Week 4 of "Python Data Analysis".
+Processing CSV files with baseball stastics.
+
+Be sure to read the project description page for further information
+about the expected behavior of the program.
+"""
+import time
 import csv
 
-def read_csv_fieldnames(filename, separator, quote):
-    """
-    Inputs:
-      filename  - name of CSV file
-      separator - character that separates fields
-      quote     - character used to optionally quote fields
-    Output:
-      A list of strings corresponding to the field names in the given CSV file.
-    """
-    with open(filename, newline='', encoding='utf-8') as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=separator, quotechar=quote)
-        for row in csvreader:
-            return row
-    return []
+##
+## Provided code from Week 3 Project
+##
 
 def read_csv_as_list_dict(filename, separator, quote):
     """
@@ -23,16 +20,17 @@ def read_csv_as_list_dict(filename, separator, quote):
       quote     - character used to optionally quote fields
     Output:
       Returns a list of dictionaries where each item in the list
-      corresponds to a row in the CSV file. The dictionaries in the
+      corresponds to a row in the CSV file.  The dictionaries in the
       list map the field names to the field values for that row.
     """
     table = []
-    with open(filename, newline='', encoding='utf-8') as csvfile:
+    with open(filename, newline='') as csvfile:
         csvreader = csv.DictReader(csvfile, delimiter=separator, quotechar=quote)
         for row in csvreader:
             table.append(row)
     return table
 
+#print(read_csv_as_list_dict('table1.csv', ",", '"'))
 def read_csv_as_nested_dict(filename, keyfield, separator, quote):
     """
     Inputs:
@@ -43,34 +41,13 @@ def read_csv_as_nested_dict(filename, keyfield, separator, quote):
     Output:
       Returns a dictionary of dictionaries where the outer dictionary
       maps the value in the key_field to the corresponding row in the
-      CSV file. The inner dictionaries map the field names to the field values for that row.
+      CSV file.  The inner dictionaries map the field names to the
+      field values for that row.
     """
     table = {}
-    with open(filename, newline='', encoding='utf-8') as csvfile:
+    with open(filename, newline='') as csvfile:
         csvreader = csv.DictReader(csvfile, delimiter=separator, quotechar=quote)
         for row in csvreader:
             rowid = row[keyfield]
             table[rowid] = row
     return table
-
-def write_csv_from_list_dict(filename, table, fieldnames, separator, quote):
-    """
-    Inputs:
-      filename   - name of CSV file
-      table      - list of dictionaries containing the table to write
-      fieldnames - list of strings corresponding to the field names in order
-      separator  - character that separates fields
-      quote      - character used to optionally quote fields
-    Output:
-      Writes the table to a CSV file with the specified name, using the specified
-      fieldnames, separator, and quote character. All non-numeric fields will be quoted.
-    """
-    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
-        csvwriter = csv.DictWriter(csvfile,
-                                   fieldnames=fieldnames,
-                                   delimiter=separator,
-                                   quotechar=quote,
-                                   quoting=csv.QUOTE_NONNUMERIC)
-        csvwriter.writeheader()
-        for row in table:
-            csvwriter.writerow(row)
